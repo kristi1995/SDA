79fb73b9f574d9a4bab7ff0a5fbdb8a163ba8609
kristi1995 <77001286+kristi1995@users.noreply.github.com>
2021-02-22 11:42:56 +0100

    Add files via upload

---
+++ generowanielosowe.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class generowanielosowe {
+    public static void main(String[] args) {
+        Random r = new Random();
+        for (int i = 0; i<10; i++)
+            System.out.println(1+r.nextInt(10));
+    }
+}
